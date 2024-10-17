b395092049dd959bdf315512843478101cd1bc8b
Elisantib <elisa.asp94@gmail.com>
2024-10-17 00:32:55 -0300

    añadiendo estilos a los componentes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,13 @@
+import './styles/TodoList.css'
+
 function TodoItem(props) {
   return (
-    <li className="list-todoitemd">
+    <li className="list-todoitem">
+      <span className='V'>V</span>
       <p>{ props.text }</p>
-      <span>V</span>
-      <span>X</span>
+      <div className='V-X'>
+        <span className='X'>X</span>
+      </div>
     </li>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 function TodoList(props) {
   return (
-    <ul>
+    <ul className="list">
       {props.children}
     </ul>
   );
